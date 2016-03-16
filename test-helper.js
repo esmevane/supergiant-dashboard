f52b8c96e57e1cafa2254d45ef798a8728497e50
Joseph McCormick <esmevane@gmail.com>
2016-03-16 13:08:51 -0500

    Apps directory cleanliness pass.
    * Set up test suite for React components.
    * Decouple containers from components where possible.
    * Add some documentation and type checking.

--- test-helper.js
+++ test-helper.js
@@ -4,12 +4,13 @@ import 'babel-polyfill'
 import React from 'react'
 import chai from 'chai'
 import chaiImmutable from 'chai-immutable'
-import { createRenderer } from 'react-addons-test-utils'
+import { createRenderer, Simulate } from 'react-addons-test-utils'
 
 chai.use(chaiImmutable)
 
 if (global) {
   global.expect = chai.expect
   global.React = React
+  global.Simulate = Simulate
   global.createRenderer = createRenderer
 }
