fe90be63ae5e061f28f6319e4ba97c70e82e7dc9
Joseph McCormick <esmevane@gmail.com>
2016-04-19 08:40:38 -0500

    Do not extract CSS in production builds.
    The separate CSS files were causing some development problems.  We'll be
    ready to add them back in later at some point, I'm sure, but for now
    it's better to just factor that behavior out for the sake of simplicity.

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,38 +1,35 @@
-var path              = require('path');
-var autoprefixer      = require('autoprefixer');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var webpack           = require('webpack');
-var meta              = { port: 9002 };
-var styles            = path.resolve(__dirname, './lib', 'styles');
-var bourbonNeat       = require('node-neat').includePaths;
-var normalizeScss     = require('node-normalize-scss').includePaths;
-var stylePaths        = [styles].concat(bourbonNeat, normalizeScss);
-var styleQueries      = [];
+var path          = require('path');
+var autoprefixer  = require('autoprefixer');
+var webpack       = require('webpack');
+var meta          = { port: 9002 };
+var styles        = path.resolve(__dirname, './lib', 'styles');
+var bourbonNeat   = require('node-neat').includePaths;
+var normalizeScss = require('node-normalize-scss').includePaths;
+var stylePaths    = [styles].concat(bourbonNeat, normalizeScss);
+var styleQueries  = [];
 
 stylePaths.forEach(function(stylePath) {
   styleQueries.push("includePaths[]=" + stylePath)
 });
 
-var sassLoaders = [
+var styleLoader = [
+  'style-loader',
   'css-loader',
   'postcss-loader',
   'sass-loader?indentedSyntax=sass&' + styleQueries.join("&")
-];
-
-var extractLoader = ExtractTextPlugin.extract(
-  'style-loader',
-  sassLoaders.join('!')
-);
+].join("!");
 
 module.exports = {
   meta:     meta,
   devtool:  'eval',
   stats:    { colors: true, modules: true, reasons: true },
   progress: true,
-  entry:    [ path.join(__dirname, 'lib', 'index.js') ],
+  entry: [
+    "font-awesome-sass!./font-awesome-sass.config.js",
+    path.join(__dirname, 'lib', 'index.js')
+  ],
   plugins:  [
     new webpack.optimize.UglifyJsPlugin({ sourceMap: false, mangle: true }),
-    new ExtractTextPlugin('application.css'),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production')
     })
@@ -53,7 +50,17 @@ module.exports = {
         include: [ path.join(__dirname, 'lib') ],
         loaders: [ 'react-hot', 'babel-loader' ],
       },
-      { test: /\.(scss|sass)$/, loader: extractLoader }
+      {
+        test: /\.(scss|sass)$/, loader: styleLoader
+      },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "url-loader?limit=10000&mimetype=application/font-woff"
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "file-loader"
+      }
     ]
   }
 };
