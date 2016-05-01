d10af3b6dcb302287fb89a3beccdcb9e10bf46e7
Joseph McCormick <esmevane@gmail.com>
2016-05-01 12:46:13 -0500

    Support 0.5+ API with a resources js client
    This commit introduces a more isolated concept of a client.  Where
    before client logic and entities were interwoven with the dashboard
    components and behavior, they are now being moved gradually to the
    `lib/resources` folder.
    
    The hope is that this decomposition will introduce a more modular client
    which can be extracted into its own Supergiant.js repository and
    probably released on NPM.
    
    This commit does NOT integrate the client with the dashboard yet, and it
    does NOT have support for app-specific resources:  containers,
    components, releases, volumes, instances and deploys will have their own
    commit.
    
    The conceptual hurdle with implementing those base resources is that
    they are all idiomatic to some degree.  Not to mention that they are
    also child resources (or completely ephemeral), and so the additional
    level of route building logic has to be considered at some point in
    order to avoid a completely wrong design.

--- test-helper.js
+++ test-helper.js
@@ -4,12 +4,39 @@ import 'babel-polyfill'
 import React from 'react'
 import chai from 'chai'
 import chaiImmutable from 'chai-immutable'
+import chaiAsPromised from 'chai-as-promised'
 import { createRenderer, Simulate } from 'react-addons-test-utils'
 
 chai.use(chaiImmutable)
+chai.use(chaiAsPromised)
+
+const mockClient = (Entity, status = 200) => {
+  const body = {}
+  const items = [body]
+  const response = { status }
+  const result = { response, body }
+  const listResult = { response, body: { items } }
+
+  const throwErrorUnless = code => clientResponse => result
+  const getResource = () => key => Promise.resolve(result)
+  const getResources = () => () => Promise.resolve({ response, listResult })
+  const createResource = () => params => Promise.resolve(result)
+  const updateResource = () => entity => Promise.resolve(entity)
+  const deleteResource = () => key => Promise.resolve({})
+
+  return {
+    throwErrorUnless,
+    getResource,
+    getResources,
+    deleteResource,
+    createResource,
+    updateResource
+  }
+}
 
 if (global) {
   global.expect = chai.expect
+  global.mockClient = mockClient
   global.React = React
   global.Simulate = Simulate
   global.createRenderer = createRenderer
